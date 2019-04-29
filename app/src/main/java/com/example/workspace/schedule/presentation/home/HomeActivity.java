36bc509e1efa9be7af522c30ef8c2217ef702db5
n.kakuta <naoki.k.0820.i@gmail.com>
2019-04-29 21:45:27 +0900

    HomeActivityのディレクトリの移動

--- app/src/main/java/com/example/workspace/schedule/presentation/home/HomeActivity.java
+++ app/src/main/java/com/example/workspace/schedule/presentation/home/HomeActivity.java
@@ -1,7 +1,9 @@
-package com.example.workspace.schedule;
+package com.example.workspace.schedule.presentation.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.workspace.schedule.R;
 
 public class HomeActivity extends AppCompatActivity {
 
